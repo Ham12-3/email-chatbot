937c5f5535863c5343a21c41dec80faab6db5b1c
Abdulhamid Sonaike <mobolaji2309@gmail.com>
2025-08-03 13:03:08 +0100

    update

--- src/components/forms/sign-up/user-type-card.tsx
+++ src/components/forms/sign-up/user-type-card.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Card, CardContent } from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { User } from "lucide-react";
 import React from "react";
 import { FieldValues, UseFormRegister } from "react-hook-form";
 
@@ -21,7 +25,34 @@ const UserTypeCard = ({
   userType,
   setUserType,
 }: Props) => {
-  return <div>UserTypeCard</div>;
+  return (
+    <Label htmlFor={value}>
+<Card className={cn(
+    'w-full cursor-pointer',
+    userType == value && 'border-orange'
+)}>
+<CardContent className="flex justify-between p-2">
+    <div className="flex items-center gap-3">
+<Card className={cn(
+    'flex justify-center p-3',
+    userType ==value && 'border-orange'
+
+)}> 
+
+<User
+size={30}
+className={cn(
+    userType == value ? 'text-orange' : 'text-gray-400'
+)}
+/>
+</Card>
+    </div>
+
+</CardContent>
+
+</Card>
+    </Label>
+  )
 };
 
 export default UserTypeCard;
